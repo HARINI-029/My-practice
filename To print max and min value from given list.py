5d13a88e2a192292f38505637a2caacc4c78ac53
HARINI PERUMAL <bestharini.29@gmail.com>
2024-10-04 17:41:05 +0530

    Add files via upload

---
+++ To print max and min value from given list.py
@@ -0,0 +1,11 @@
+#To print max and min value from given list#
+list1=[]
+for num in range(5):
+    value = int(input())
+    list1.append(value)
+
+print("The maximum value is:")
+print(max(list1))
+
+print("The maximum value is:")
+print(min(list1))
