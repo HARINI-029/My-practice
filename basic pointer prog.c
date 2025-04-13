f62b037e7f4c16aa3ee56a4ecef7c403773ebc1d
HARINI PERUMAL <bestharini.29@gmail.com>
2025-04-13 07:11:40 +0530

    Add files via upload

---
+++ basic pointer prog.c
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int v=20;
+   int *p=&v;
+   printf("%d\n%p",*p,p);
+}
